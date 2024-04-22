d55efae3e7cb1af4f0f8de3a13066672674c34e5
yuan <whyuan00@gmail.com>
2024-04-22 17:52:31 +0900

    update

--- 06pjt/ver1_skeleton/accounts/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.conf import settings
-
-class User(AbstractUser):
-    followings = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followers',symmetrical=False)
-    nickname = models.CharField(max_length=30)
-    
