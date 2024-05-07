5849beea543fbf1c6e5172bc9c207ac7a9b49dea
yuan <whyuan00@gmail.com>
2024-05-07 17:25:20 +0900

    update

---
+++ vue/04-component-state-flow/vue-project/src/components/Parentgrandchild.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>
+            {{ myMsg }}
+        </p>
+        <button @click="updateName">이름변경</button>
+     
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    myMsg: String
+})
+
+const emit = defineEmits(['updateName'])
+const updateName = function(){
+    emit('updateName')
+}
+</script>
+
+<style  scoped>
+
+</style>
