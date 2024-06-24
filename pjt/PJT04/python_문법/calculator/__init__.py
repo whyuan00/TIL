08e92495205550ed77242b5a841645ce5957af08
yuan <whyuan@gmail.com>
2024-06-24 15:07:38 +0900

    update

--- pjt/PJT04/python_문법/calculator/__init__.py
+++
@@ -1,8 +0,0 @@
-number = 10
-
-def add(a, b):
-    return a + b
-
-
-def subtract(a, b):
-    return a - b
--- pjt/PJT04/python_문법/calculator/adv.py
+++
@@ -1,9 +0,0 @@
-def multiply(a, b):
-    return a * b
-
-
-def divide(a, b):
-    if b == 0:
-        return "0으로 나누지 마라"
-    return a / b
-
